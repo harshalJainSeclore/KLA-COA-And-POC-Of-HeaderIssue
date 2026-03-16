ceea31b6e3e00e19ea928218fa70632cee8ffce0
Sanjeev Dhakad <sanjeev.dhakad@seclore.com>
2026-03-16 18:03:02 +0530

    added register offline comp

--- SetupHelper.cpp
+++ SetupHelper.cpp
@@ -10,7 +10,9 @@
 namespace
 {
 using RegisterComponentExWFn = long (*)(ComponentInfoExW*, wchar_t*);
+using RegisterOffIdEstComponentWFn = long (*)(OfflineIdEstCompInfoW*, wchar_t*);
 using UnregisterComponentWFn = long (*)(wchar_t*, wchar_t*);
+using UnregisterOffIdEstComponentWFn = long (*)(wchar_t*, wchar_t*);
 
 constexpr wchar_t kDllPath[] = L"C:\\Program Files (x86)\\Seclore\\FileSecure\\Desktop Client\\setuphelper.dll";
 constexpr wchar_t kComponentId[] = L"{33EB9B69-0CC4-4561-8E50-B91E8536B2AD}";
@@ -25,6 +27,12 @@ constexpr wchar_t kResCollFolder[] = L"Resources";
 constexpr wchar_t kUrl[] = L"/downloads/mycomponent";
 constexpr wchar_t kUninstallString[] = L"{2DC55591-1FF4-471F-B0F5-B5254DF734D0}";
 constexpr wchar_t kCurrentVersion[] = L"1.0.0.0";
+constexpr wchar_t kOfflineComponentId[] = L"{33EB9B69-0CC4-4561-8E50-B91E8536B2AD}";
+constexpr wchar_t kOfflineComponentName[] = L"My Offline Component";
+constexpr wchar_t kOfflineComponentDescription[] = L"My offline identity establishment component";
+constexpr wchar_t kOfflineComponentPath[] = L"C:\\Program Files (x86)\\Seclore\\FileSecure\\Desktop Client\\Addon\\OfflineAgent\\OfflineAgent.dll";
+constexpr wchar_t kOfflineComponentPath64[] = L"C:\\Program Files\\Seclore\\FileSecure\\Desktop Client\\Addon\\OfflineAgent\\OfflineAgent.dll";
+constexpr DWORD kOfflineOptions = OPTION_MASK_INTERACTIVE | OPTION_UNMASK_INTERACTIVE;
 
 void copyToBuffer(const wchar_t* source, wchar_t* destination, std::size_t destinationCount)
 {
@@ -64,6 +72,24 @@ ComponentInfoExW createComponentInfo()
     return info;
 }
 
+OfflineIdEstCompInfoW createOfflineComponentInfo()
+{
+    OfflineIdEstCompInfoW info{};
+    info.mlSize = OFFLINE_ID_EST_COMP_INFOW_VER3;
+    info.mlOptions = kOfflineOptions;
+    info.mOfflineCompConfig.mlSize = OFFLINE_COMP_CONFIGW_VER1_SIZE;
+
+    copyToBuffer(kOfflineComponentId, info.mstrId, std::size(info.mstrId));
+    copyToBuffer(kOfflineComponentPath, info.mstrPath, std::size(info.mstrPath));
+    copyToBuffer(kOfflineComponentName, info.mstrName, std::size(info.mstrName));
+    copyToBuffer(kOfflineComponentDescription, info.mstrDesc, std::size(info.mstrDesc));
+    copyToBuffer(kVersion, info.mstrVersion, std::size(info.mstrVersion));
+    copyToBuffer(kInstallPath, info.mstrInstallDir, std::size(info.mstrInstallDir));
+    copyToBuffer(kOfflineComponentPath64, info.mstrPath64, std::size(info.mstrPath64));
+
+    return info;
+}
+
 template <typename FunctionPointer>
 FunctionPointer getFunction(HMODULE module, const char* name)
 {
@@ -83,17 +109,48 @@ int registerComponent(HMODULE module)
     wchar_t error[ERROR_MSG_MAX_LEN + 1]{};
     const long result = registerFn(&info, error);
 
-    std::wcout << L"Register result: " << result << L'\n';
+    std::wcout << L"Register component result: " << result << L'\n';
     if (error[0] != L'\0')
     {
-        std::wcout << L"Register error: " << error << L'\n';
+        std::wcout << L"Register component error: " << error << L'\n';
     }
 
-    return result == SETUP_SUCCESS ? 0 : static_cast<int>(result);
+    if (result != SETUP_SUCCESS)
+    {
+        return static_cast<int>(result);
+    }
+
+    const auto registerOfflineFn =
+        getFunction<RegisterOffIdEstComponentWFn>(module, "IRegisterOffIdEstComponentW");
+    if (!registerOfflineFn)
+    {
+        std::cerr << "Unable to locate export: IRegisterOffIdEstComponentW\n";
+        return 1;
+    }
+
+    OfflineIdEstCompInfoW offlineInfo = createOfflineComponentInfo();
+    std::wmemset(error, 0, std::size(error));
+    const long offlineResult = registerOfflineFn(&offlineInfo, error);
+
+    std::wcout << L"Register offline component result: " << offlineResult << L'\n';
+    if (error[0] != L'\0')
+    {
+        std::wcout << L"Register offline component error: " << error << L'\n';
+    }
+
+    return offlineResult == SETUP_SUCCESS ? 0 : static_cast<int>(offlineResult);
 }
 
 int unregisterComponent(HMODULE module)
 {
+    const auto unregisterOfflineFn =
+        getFunction<UnregisterOffIdEstComponentWFn>(module, "IUnregisterOffIdEstComponentW");
+    if (!unregisterOfflineFn)
+    {
+        std::cerr << "Unable to locate export: IUnregisterOffIdEstComponentW\n";
+        return 1;
+    }
+
     const auto unregisterFn = getFunction<UnregisterComponentWFn>(module, "IUnregisterComponentW");
     if (!unregisterFn)
     {
@@ -101,16 +158,33 @@ int unregisterComponent(HMODULE module)
         return 1;
     }
 
+    wchar_t error[ERROR_MSG_MAX_LEN + 1]{};
+    wchar_t offlineComponentId[MAX_OFFLINE_COMP_ID_LEN + 1]{};
+    copyToBuffer(kOfflineComponentId, offlineComponentId, std::size(offlineComponentId));
+
+    const long offlineResult = unregisterOfflineFn(offlineComponentId, error);
+
+    std::wcout << L"Unregister offline component result: " << offlineResult << L'\n';
+    if (error[0] != L'\0')
+    {
+        std::wcout << L"Unregister offline component error: " << error << L'\n';
+    }
+
+    if (offlineResult != SETUP_SUCCESS)
+    {
+        return static_cast<int>(offlineResult);
+    }
+
     wchar_t componentId[MAX_COMP_ID_LEN + 1]{};
     copyToBuffer(kComponentId, componentId, std::size(componentId));
 
-    wchar_t error[ERROR_MSG_MAX_LEN + 1]{};
+    std::wmemset(error, 0, std::size(error));
     const long result = unregisterFn(componentId, error);
 
-    std::wcout << L"Unregister result: " << result << L'\n';
+    std::wcout << L"Unregister component result: " << result << L'\n';
     if (error[0] != L'\0')
     {
-        std::wcout << L"Unregister error: " << error << L'\n';
+        std::wcout << L"Unregister component error: " << error << L'\n';
     }
 
     return result == SETUP_SUCCESS ? 0 : static_cast<int>(result);
